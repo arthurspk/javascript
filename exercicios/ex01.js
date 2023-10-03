c48b1f60ad4ed59b02fa64b76b3eacf6eb153349
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-03 09:16:31 -0300

    Update ex01.js
    Este código em JavaScript cria um conjunto de informações sobre uma pessoa chamada "Arthur" e seus times de futebol menos preferidos. Ele utiliza objetos para armazenar os dados da pessoa e dos times de futebol e define uma função chamada "resultado" para imprimir essas informações no console.

--- exercicios/ex01.js
+++ exercicios/ex01.js
@@ -1,11 +1,33 @@
-var nome = "Arthur";
-var idade = 20;
+// Definindo um objeto chamado 'pessoa' com informações sobre uma pessoa
+const pessoa = {
+  nome: "Arthur", // Nome da pessoa
+  idade: 20, // Idade da pessoa
+  comidaFavorita: "Pizza", // Comida favorita da pessoa
+};
 
-var comidaFavorita;
-comidaFavorita = "pizza";
+// Definindo um objeto chamado 'obj' com nomes de times de futebol
+const obj = {
+  timePrimario: "Flamengo", // Time de futebol primário
+  timeSecundario: "Vasco", // Time de futebol secundário
+  timeTerciario: "Fluminense", // Time de futebol terciário
+  timeQuartenario: "Botafogo", // Time de futebol quartário
+};
 
-var timePrimario,
-    timeSecundario,
-    timeTerciario,
-    timeQuartenario,
-    timeQuintenario;
+// Unindo os nomes dos times em uma única string separada por vírgula
+const timesOdiados = [
+  obj.timePrimario,
+  obj.timeSecundario,
+  obj.timeTerciario,
+  obj.timeQuartenario,
+].join(", ");
+
+// Definindo uma função chamada 'resultado'
+const resultado = () => {
+  // Exibindo informações sobre a pessoa e os times que ela odeia no console
+  return console.log(
+    `\nNome: ${pessoa.nome}\nIdade: ${pessoa.idade}\n\nTimes que ${pessoa.nome} ODEIA: ${timesOdiados}.`
+  );
+};
+
+// Chamando a função 'resultado' para mostrar os resultados no console
+resultado();
